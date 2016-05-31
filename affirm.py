9ddc068d70d5b01fa37cdda565360de07c750fb5
Eli Finer <eli.finer@gmail.com>
2016-05-31 11:35:10 -0700

    initial

---
+++ affirm.py
@@ -0,0 +1,83 @@
+# affirm.py / Eli Finer / 2016
+#
+# This script causes assert statements to output much better default error messages
+# that include the tested condition and the values for any variables referenced in it.
+#
+# Here are some examples:
+#
+# >>> assert 1 > 2
+# AssertionError: assertion (1 > 2) failed
+#
+# >>> a = 1; b = 2; c = 'foo'; d = None
+# >>> assert a > b
+# AssertionError: assertion (a > b) failed with a=1, b=2
+#
+# >>> assert c is None
+# AssertionError: assertion (c is None) failed with c='foo'
+#
+# >>> assert a == b == c
+# AssertionError: assertion (a == b == c) failed with a=1, b=2, c='foo'
+
+def make_assert_message(frame):
+    import ast
+    import inspect
+    from collections import OrderedDict
+
+    class ReferenceFinder(ast.NodeVisitor):
+        def __init__(self):
+            self.names = []
+        def find(self, tree, frame):
+            self.visit(tree)
+            deref = OrderedDict()
+            for name in self.names:
+                if name in frame.f_locals:
+                    deref[name] = repr(frame.f_locals[name])
+                elif name in frame.f_globals:
+                    deref[name] = repr(frame.f_globals[name])
+            return deref
+        def visit_Name(self, node):
+            self.names.append(node.id)
+
+    code = inspect.getframeinfo(frame)[3][0]
+    condition = code.strip()[len('assert '):]
+    deref = ReferenceFinder().find(ast.parse(condition), frame)
+    deref_str = ''
+    if deref:
+        deref_str = ' with ' + ', '.join('{}={}'.format(k, v) for k, v in deref.items())
+    return 'assertion ({}) failed{}'.format(condition, deref_str)
+
+import sys
+_old_excepthook = sys.excepthook
+def assert_excepthook(type, value, traceback):
+    '''
+    >>> assert 1 > 2
+    Traceback (most recent call last):
+    AssertionError: assertion (1 > 2) failed
+    '''
+    if type == AssertionError:
+        from traceback import print_exception
+        if not value.args:
+            top = traceback
+            while top.tb_next:
+                top = top.tb_next
+            value = AssertionError(make_assert_message(top.tb_frame))
+        print_exception(type, value, traceback)
+    else:
+        _old_excepthook(type, value, traceback)
+sys.excepthook = assert_excepthook
+
+def foo():
+    assert 1 > 2
+
+def main():
+    foo()
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    # main()
+    # assert 1 > 2
+    # a = 1; b = 2; c = 'foo'; d = None
+    # assert a > b
+    # assert c is None
+    # assert a == b == c
