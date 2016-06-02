1effacd7cd222c1a6582209946e870e50d3ff5e0
Eli Finer <eli.finer@gmail.com>
2016-06-02 13:36:16 -0700

    added affirm() function
    This function should be used when there's a need to catch the
    AssertionError in Python code and get the smart message.

--- affirm.py
+++ affirm.py
@@ -18,11 +18,12 @@
 # >>> assert a == b == c
 # AssertionError: assertion (a == b == c) failed with a=1, b=2, c='foo'
 
-def make_assert_message(frame):
-    import ast
-    import inspect
-    from collections import OrderedDict
+import re
+import ast
+import inspect
+from collections import OrderedDict
 
+def make_assert_message(frame, regex):
     class ReferenceFinder(ast.NodeVisitor):
         def __init__(self):
             self.names = []
@@ -41,31 +42,35 @@ def make_assert_message(frame):
     code_context = inspect.getframeinfo(frame)[3]
     if not code_context:
         return ''
-    else:
-        code = code_context[0]
-        condition = code.strip()[len('assert '):]
-        deref = ReferenceFinder().find(ast.parse(condition), frame)
-        deref_str = ''
-        if deref:
-            deref_str = ' with ' + ', '.join('{}={}'.format(k, v) for k, v in deref.items())
-        return 'assertion ({}) failed{}'.format(condition, deref_str)
+    match = re.search(regex, code_context[0])
+    if not match:
+        return ''
+    condition = match.group(1).strip()
+    deref = ReferenceFinder().find(ast.parse(condition), frame)
+    deref_str = ''
+    if deref:
+        deref_str = ' with ' + ', '.join('{}={}'.format(k, v) for k, v in deref.items())
+    return 'assertion ({}) failed{}'.format(condition, deref_str)
 
 import sys
 _old_excepthook = sys.excepthook
 def assert_excepthook(type, value, traceback):
-    '''
-    >>> assert 1 > 2
-    Traceback (most recent call last):
-    AssertionError: assertion (1 > 2) failed
-    '''
     if type == AssertionError:
         from traceback import print_exception
         if not value.args:
             top = traceback
             while top.tb_next and top.tb_next.tb_frame:
                 top = top.tb_next
-            value = AssertionError(make_assert_message(top.tb_frame))
+            message = make_assert_message(top.tb_frame, r'assert\s+([^#]+)')
+            value = AssertionError(message)
         print_exception(type, value, traceback)
     else:
         _old_excepthook(type, value, traceback)
 sys.excepthook = assert_excepthook
+
+def affirm(condition):
+    if condition:
+        return
+    else:
+        message = make_assert_message(inspect.currentframe().f_back, r'affirm\s*\(\s*(.+)\s*\)')
+        raise AssertionError(message)
